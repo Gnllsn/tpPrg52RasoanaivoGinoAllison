89bfa744933aeb547ccd3dc6482b3a1980dce2d2
Gino Allison <77733677+Gnllsn@users.noreply.github.com>
2023-04-01 17:55:18 +0300

    Exercice 3 : utilisation des paramètres de vue

--- src/main/java/jsf/Bean.java
+++ src/main/java/jsf/Bean.java
@@ -27,7 +27,7 @@ public class Bean {
     }
     
     public String afficher() {
-       return "affichage_3?nb="+ nombre + "&amp;faces-redirect=true";
+        return "affichage_3?nb="+ nombre + "&amp;faces-redirect=true";
     }
 
     public List<Integer> getNombresSuivants() {
